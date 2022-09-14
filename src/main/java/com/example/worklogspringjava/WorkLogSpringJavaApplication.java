b9efb444c5629ddce5cb955a7280288d709b1141
Granchey <96749754+Granchey@users.noreply.github.com>
2022-09-14 15:42:29 +0200

    Add files via upload

---
+++ src/main/java/com/example/worklogspringjava/WorkLogSpringJavaApplication.java
@@ -0,0 +1,19 @@
+package com.example.worklogspringjava;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WorkLogSpringJavaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkLogSpringJavaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
